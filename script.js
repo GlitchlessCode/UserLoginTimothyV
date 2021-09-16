1a415ea2d283e02471225aa3ba2e59da0a62606a
TurtleY0da <timothy.veugelers@gmail.com>
2021-09-16 13:45:42 -0600

    Current Final Commit for User Login Assignment

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 // User Login Script by Timothy V
 
 // Initialize Variables
+let userIn = document.getElementById("userIn");
+let passIn = document.getElementById("passIn");
+let loginBtn = document.getElementById("loginBtn");
 
 // Add Event Listener
+loginBtn.addEventListener("click", checkLogin);
 
 // Functions
 function checkLogin() {
-    // Input
 
     // Process & Output
+    if (userIn.value !== "admin") {
+        alert("Invalid Username");
+
+    } else if (passIn.value !== "1234") {
+        alert("Invalid Password");
+
+    } else {
+        alert("Login Successful");
+
+    }
 }
